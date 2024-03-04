daf6a8f46681a3216ca753659ee8cb4210f96754
lucasbarroso23 <lucasbarroso23@gmail.com>
2024-03-04 22:39:39 +0000

    feat: update handler structure

---
+++ handler/handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ShowOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "GET opening",
+	})
+}
+
+func CreateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "POST opening",
+	})
+}
+
+func DeleteOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "DELETE opening",
+	})
+}
+
+func UpdateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "PUT opening",
+	})
+}
+
+func ListOpeningsHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "GET openings",
+	})
+}
