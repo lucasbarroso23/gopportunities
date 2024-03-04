2d79b477fd27e67f19608c9d90ca425ab9430061
lucasbarroso23 <lucasbarroso23@gmail.com>
2024-03-04 23:42:28 +0000

    feat: setup logger

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/lucasbarroso23/gopportunities/router"
+import (
+	"github.com/lucasbarroso23/gopportunities/config"
+	"github.com/lucasbarroso23/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.NewLogger("main")
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
