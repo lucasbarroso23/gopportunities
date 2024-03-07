137861bf77064a8bd0c7f276918e02a6c1829ca0
lucasbarroso23 <lucasbarroso23@gmail.com>
2024-03-07 23:11:54 +0000

    feat: list, show and update handlers

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -4,10 +4,22 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucasbarroso23/gopportunities/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusNotFound, errParamIsRequired("id", "queryParamenter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
